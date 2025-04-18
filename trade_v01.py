e8d9ceeed6b4422824ecb8963f204b186592485d
Murti <bozyigit31@gmail.com>
2025-04-18 06:45:07 +0300

    İzole açıuorum işlemleri artık. NİCEEEUUU

--- trade_v01.py
+++ trade_v01.py
@@ -9,6 +9,7 @@ import time
 import threading
 from tkinter import ttk, messagebox
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from helpers.binance_isolated import set_isolated_mode
 from indicators.harmonic import harmonic_xabcd_validate
 #from folders.ema.emaMurtaza import EMA
 from datetime import datetime, timedelta,timezone
@@ -253,8 +254,9 @@ def resume_refresh(event): should_auto_refresh.set(True)
 def execute_trade():
     raw_symbol = symbol_var.get().strip().upper()
     symbol = raw_symbol if "/" in raw_symbol else raw_symbol + "/USDT"
+    binance_symbol = symbol.replace("/", "")
     timeframe = timeframe_var.get()
-
+    set_isolated_mode('irGpxO0nbn4jKHNqddCdaBQS14L9XJ5NxMBgLlg6vBrwMqGAGlqyjqJb6prmAP42', '6ZxTkk6CEeeWWHdSUwgduUbXQ8Jw1IL6GN7NTOt95fgoFktPC0qYM1GfhK8VbAag', binance_symbol)
     df = get_ohlcv(symbol, timeframe)
     if df is None or df.empty:
         messagebox.showwarning("Uyarı", "İşlem için geçerli veri alınamadı!")
