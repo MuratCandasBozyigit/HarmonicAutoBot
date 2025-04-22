6be8c067d5598f435c79d5369fa2153a7076d543
Murti <bozyigit31@gmail.com>
2025-04-22 21:58:42 +0300

    h

--- DrawPattern/drawHarmonic.py
+++ DrawPattern/drawHarmonic.py
@@ -41,7 +41,7 @@ def draw_bullish_patterns(df, ax):
 
                 Cmd.add_log(f"[Harmonic] BULLISH {detected} bulundu @ index {dX}")
 
-                if dX == len(df) -7 and globals.emir_acik:
+                if dX == len(df) - 2 and globals.emir_acik:
                     pattern_id = hash((round(xY, 2), round(aY, 2), round(bY, 2), round(cY, 2), round(dY, 2)))
                     if pattern_id not in opened_patterns:
                         Order.open_position(dY, globals.symbol)
