b5991128849b8eabab0b46be82921e56b7b91bd9
Murti <bozyigit31@gmail.com>
2025-04-23 18:32:15 +0300

    k

--- Utils/__init__.py
+++ Utils/__init__.py
@@ -1,3 +1,4 @@
 ﻿from .binance_isolated import set_isolated_mode
 from .globals import *
 from .get_ohlcv import get_ohlcv
+from .messagebox_utils import *
---
+++ Utils/messagebox_utils.py
@@ -0,0 +1,15 @@
+﻿import tkinter as tk
+from tkinter import Toplevel, Label
+
+def show_timed_message(title, message, duration=5000):  # duration = milisaniye
+    win = Toplevel()
+    win.title(title)
+    win.geometry("300x100")
+    win.configure(bg="#fefefe")
+    win.attributes("-topmost", True)
+
+    label = Label(win, text=message, font=("Arial", 11), wraplength=280, bg="#fefefe")
+    label.pack(pady=20)
+
+    # 5 saniye sonra pencereyi otomatik kapat
+    win.after(duration, win.destroy)
