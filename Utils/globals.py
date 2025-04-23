d10a81af88e49603c7378fa819d835cfbe043101
Murti <bozyigit31@gmail.com>
2025-04-23 14:31:49 +0300

    gg

--- Utils/globals.py
+++ Utils/globals.py
@@ -14,7 +14,7 @@ should_auto_refresh = None  # <-- Burayı None bırak
 
 # Diğer değişkenler
 emir_acik = False
-short_emir_acik=True
+short_emir_acik=False
 aktif_emir_id = None
 last_candle_time = None
 df = None
