15cf19fed953f6e3b566b92528925e578af18a79
Murti <bozyigit31@gmail.com>
2025-04-24 13:29:22 +0300

    s

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import Cmd
 import Order
 import Chart
 import Gui
-
+import sys
 if __name__ == "__main__":
     root = tk.Tk()
+    root.protocol("WM_DELETE_WINDOW", lambda: sys.exit())
     globals.root = root
     Gui.build_gui(root)
     root.mainloop()
